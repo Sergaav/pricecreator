62bbb38b9f891bfbc45fff686c1fd06eaaa5ac5b
Andrii Serha <sergaav@gmail.com>
2021-03-06 13:39:37 +0200

    XML parsing done

--- src/main/java/model/XMLReader.java
+++ src/main/java/model/XMLReader.java
@@ -1,90 +1,113 @@
 package model;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.xml.sax.Attributes;
+import org.jdom2.*;
+import org.jdom2.input.DOMBuilder;
+import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.DOMBuilder;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.input.StAXEventBuilder;
+import org.jdom2.input.StAXStreamBuilder;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class XMLReader {
 
-    private final SAXParser parser;
+
     public static Queue<Item> itemQueue = new ConcurrentLinkedQueue<>();
 
 
-    public XMLReader() throws ParserConfigurationException, SAXException {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        parser = factory.newSAXParser();
-
+    public XMLReader() {
 
     }
 
-    public Queue<Item> getItemQueueFromXML(String fileName) throws IOException, SAXException {
-        XMLHandler handler = new XMLHandler();
-        parser.parse(new File(fileName), handler);
+    public Queue<Item> getItemQueueFromXML(String fileName) {
+        try {
+            org.jdom2.Document document = createJDOMusingSAXParser(fileName);
+            Element root = document.getRootElement();
+
+            List<Element> ItemElements = root.getChildren("Прайс");
+            for (Element element : ItemElements) {
+                Item item = new Item();
+
+                item.setCart(element.getChild("KART").getValue().trim());
+
+                item.setNameRus(element.getChild("NAIM").getValue().trim());
+                item.setNameUkr(element.getChild("NAIMUKR").getValue().trim());
+                item.setItemCatNumber(element.getChild("KODKAT").getValue().trim());
+
+                item.setPriceIn(Double.valueOf(element.getChild("CENAPART").getValue().trim().replace(',','.')));
+
+                item.setBrand(element.getChild("PROIZVODIT").getValue().trim());
+                item.setWeight(Double.parseDouble(element.getChild("MASSA").getValue().trim().replace(',','.')));
+
+                itemQueue.add(item);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JDOMException e) {
+            e.printStackTrace();
+        }
+
         return itemQueue;
+    }
+
+    private static org.jdom2.Document createJDOMusingDOMParser(String fileName)
+            throws ParserConfigurationException, SAXException, IOException {
+        //создаем DOM Document
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder;
+        documentBuilder = dbFactory.newDocumentBuilder();
+        Document doc = documentBuilder.parse(new File(fileName));
+        DOMBuilder domBuilder = new DOMBuilder();
+
+        return domBuilder.build((Document) doc);
 
     }
 
-    private static class XMLHandler extends DefaultHandler {
-        String lastElementName;
+    private static org.jdom2.Document createJDOMusingSAXParser(String fileName)
+            throws JDOMException, IOException {
+        SAXBuilder saxBuilder = new SAXBuilder();
+        return saxBuilder.build(new File(fileName));
+    }
 
-        @Override
-        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-            if (qName.equals("Прайс"))
-                lastElementName = qName;
-
-
-//
-//                item.setItemCatNumber(attributes.getValue("KODKAT"));
-//                item.setNameRus(attributes.getValue("NAIM"));
-//                item.setNameUkr(attributes.getValue("NAIMUKR"));
-//                item.setBrand(attributes.getValue("PROIZVODIT"));
-//                item.setPriceIn(Double.parseDouble(attributes.getValue("CENAPART")));
-//                item.setWeight(Double.parseDouble(attributes.getValue("MASSA")));
-//                itemQueue.add(item);
-            System.out.println(attributes.getType("KART"));
-            System.out.println(attributes.getType("KODKAT"));
-            System.out.println(attributes.getType("NAIM"));
+    // получаем экземпляр JDOM Document с помощью STAX Stream Parser или STAX Event Parser
+    private static org.jdom2.Document createJDOMusingSTAXParser(String fileName, String type)
+            throws FileNotFoundException, XMLStreamException, JDOMException {
+        if (type.equalsIgnoreCase("stream")) {
+            StAXStreamBuilder staxBuilder = new StAXStreamBuilder();
+            XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+            XMLStreamReader xmlStreamReader =
+                    xmlInputFactory.createXMLStreamReader(new FileInputStream(fileName));
+            return staxBuilder.build(xmlStreamReader);
         }
-
-
-        @Override
-        public void endElement(String uri, String localName, String qName) throws SAXException {
-            if ((name != null && !name.isEmpty()) && (job != null && !job.isEmpty())) {
-                employees.add(new Employee(name, job));
-                name = null;
-                job = null;
-            }
-        }
-
-        @Override
-        public void characters(char[] ch, int start, int length) throws SAXException {
-
-            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-            Item item = context.getBean("myItem", Item.class);
-
-            String information = new String(ch, start, length);
-
-            information = information.replace("\n", "").trim();
-
-            if (!information.isEmpty()) {
-                if (lastElementName.equals("KART"))
-                    item.setCart(Integer.parseInt(information));
-                if (lastElementName.equals("KODKAT"))
-                    item.setItemCatNumber(information);
-
-            }
-        }
+        StAXEventBuilder staxBuilder = new StAXEventBuilder();
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(
+                new FileInputStream(fileName));
+        return staxBuilder.build(xmlEventReader);
 
     }
+
 
 }
 
